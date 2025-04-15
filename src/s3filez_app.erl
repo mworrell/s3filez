2af3cca3018846bbc84c462b81cc9ffb649bd305
Marc Worrell <marc@worrell.nl>
2025-04-15 08:25:21 +0200

    Add tls_options, change config argument

--- src/s3filez_app.erl
+++ src/s3filez_app.erl
@@ -1,9 +1,10 @@
-%% @doc Main app code. Ensure the httpc profiles are present, start the
-%% supervisor for the queued jobs.
 %% @author Marc Worrell
 %% @copyright 2013-2020 Marc Worrell
+%% @doc Main app code. Ensure the httpc profiles are present, start the
+%% supervisor for the queued jobs.
+%% @end
 
-%% Copyright 2013-2020 Marc Worrell
+%% Copyright 2013-2025 Marc Worrell
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
